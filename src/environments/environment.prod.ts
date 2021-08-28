5ec46dd8623f78a937d2525012f415e2eae0a546
mFlaifel <msf-2oo7@hotmail.com>
2021-08-28 18:21:36 +0300

    deploy the app https://kanbanfire-4317d.web.app/

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCRvUmRk8rAmyai2lChqvpLoWE_YMxIfBk',
+    authDomain: 'kanbanfire-4317d.firebaseapp.com',
+    projectId: 'kanbanfire-4317d',
+    storageBucket: 'kanbanfire-4317d.appspot.com',
+    messagingSenderId: '559148552394',
+    appId: '1:559148552394:web:685f19262039df63e82433',
+    measurementId: 'G-F6S8RB3L2S',
+  },
 };
